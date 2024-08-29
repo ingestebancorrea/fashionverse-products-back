18dbabef7175aae56b7de3a82db9930d83ea9942
Esteban Correa <estebancorrea405@gmail.com>
2024-08-29 17:40:18 -0500

    Agregando image_url en schema de ejemplo

--- src/products/schema/product-with-objects.schema.ts
+++ src/products/schema/product-with-objects.schema.ts
@@ -1,6 +1,7 @@
 export const ProductWithObjctsSchema = {
     "id": 1,
     "name": "Camiseta Nike Oversize",
+    "image_url": "https://image",
     "category": {
         "id": 1,
         "name": "Camiseta"
